6e17cfabdcfd2172c873db007790054c9ee7a0e9
Benita Michael <benubiaka@gmail.com>
2023-08-16 15:38:52 -0700

    advanced task

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    mult = 0
+    add = 0
+
+    for tup in my_list:
+        mult += tup[0] * tup[1]
+        add += tup[1]
+
+    return (mult / add)
