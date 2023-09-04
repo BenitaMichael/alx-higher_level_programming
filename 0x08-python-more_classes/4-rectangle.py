421c25dac114ca6233191eef9b8a7e29625c0232
Benita Michael <benubiaka@gmail.com>
2023-09-04 04:52:44 -0700

    Task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -45,7 +45,6 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-
     def area(self):
         """returns the area"""
         return (self.__width * self.__height)
@@ -55,7 +54,6 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
-
 
     def __str__(self) -> str:
         """displays the diagram of the rectangle"""
