76841270769539e805d130cbee5c299ad9be9946
Benita Michael <benubiaka@gmail.com>
2023-09-06 00:37:50 -0700

    all

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,30 +1,33 @@
-#include "Python.h"
-#include <stdio>
+#include <stdio.h>
+#include <string.h>
+#include <Python.h>
 
 /**
- * print_python_string - Prints information about python strings
- * @p: A PyObject string object
+ * print_python_string - Printsinformation about python strings
+ * @p: Python Object
  */
 
 void print_python_string(PyObject *p)
 {
-	long int len;
 
-	fflush(stdout);
+	PyObject *str, *repr;
 
+	(void)repr;
 	printf("[.] string object info\n");
-	if (strcmp(p->ob_type->tp_name, "str") != 0)
+
+	if (strcmp(p->ob_type->tp_name, "str"))
 	{
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 
-	len = ((PyASCIIObject *)(p))->len;
-
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", len);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &len));
+
+	repr = PyObject_Repr(p);
+	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
+	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
+	printf("  value: %s\n", PyBytes_AsString(str));
 }
