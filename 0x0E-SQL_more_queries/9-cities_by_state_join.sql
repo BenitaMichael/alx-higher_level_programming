2e5e0e23ab4d50dc75b79cee8ab82938481116ff
Benita Michael <benubiaka@gmail.com>
2023-10-18 08:16:55 -0700

    Task

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
--- cities contained in the database hbtn_0d_usa
-SELECT cities.id cities.name states.name
-FROM cities
-JOIN states ON cities.state_id = states.id;
+-- cities contained in the database hbtn_0d_us
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE states.id = cities.state_id
+ORDER BY cities.id ASC;
