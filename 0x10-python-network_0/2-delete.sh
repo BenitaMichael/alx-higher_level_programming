97621974827eabf5b3b0952cfa057f930ce59fa6
BenitaMichael <107327075+BenitaMichael@users.noreply.github.com>
2023-12-01 01:58:07 +0100

    changes

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a DELETE request to a URL
-curl -sX DELETE "$1"
+curl -s -X DELETE "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL as an argument, sends a GET request to the URL
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
